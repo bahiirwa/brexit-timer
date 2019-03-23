930f1ecdc7f1862325de2e5e7874161fa45b3fce
Laurence Bahiirwa <laurence@mediauganda.com>
2019-03-23 20:52:59 +0300

    first commit

---
+++ brexit-timer.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Plugin Name: Brexit Timer
+ * Description: Keep your readers upto date with the upcoming event - Brexit. Just add [brexit] to your post or page
+ * Version: 0.1.0
+ * Author: Laurence Bahiirwa
+ * Author URI: https://omukiguy.com
+ * Plugin URI: https://github.com/bahiirwa/brexittimer
+ * Text Domain: brexittimer
+ * 
+ * This is free software released under the terms of the General Public License,
+ * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Full
+ * text of the license is available at https://www.gnu.org/licenses/gpl-2.0.txt.
+ */
+
+// Prevent direct access.
+if (!defined('ABSPATH')) { die(); }
+
+class BrexitTimer {
+
+	/**
+	 * Process shortcode - This public function processes the brexit_timer shortcode into HTML markup.
+	 *
+	 * @author Laurence Bahiirwa
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $atts Shortcode arguments.
+	 * @return string Markup of shortcode output.
+	 */
+	public function process_shortcode($atts, $content = null) {
+
+        return '<span id="brexit-timer" class="brexit-timer"></span>';
+	}
+
+    /**
+	 * Enqueue scripts for plugin
+	 *
+	 * @author Laurence Bahiirwa
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string null
+	 */
+	public function brexit_timer_scripts($atts) {
+
+        $vars = shortcode_atts( array(
+            'wording'   => 'Brexit',
+            'date'  => '2019-04-12 23:00:00'
+        ), $atts );
+
+
+	    //Get UL timezone GMT+0
+        $gmt_date = gmdate('M d, Y H:i:s', strtotime($vars['date']) );
+
+        // Localize the script with new data
+        $translation_array = array(
+            'wording' => __( $vars['wording'], 'brexittimer' ),
+            'uk_time'     => $gmt_date
+        );
+
+        wp_register_script( 'brexit_timer_script', plugin_dir_url( __FILE__ ) . 'js/timer.js' );
+        wp_localize_script( 'brexit_timer_script', 'brexit_string', $translation_array );
+        wp_enqueue_script( 'brexit_timer_script');
+
+    }
+    /**
+	 * Run the functions register in the class.
+	 *
+	 * @author Laurence Bahiirwa
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string null
+	 */
+	public function register() {
+        
+        //Enqueue the scripts with localization.
+        add_action('wp_enqueue_scripts', array($this,'brexit_timer_scripts'));
+
+        // Add action to process shortcodes.
+        add_shortcode('brexit_timer', array($this, 'process_shortcode'));
+
+    }
+
+}
+
+/**
+ * Load the class in a variable.
+ * Instatiate default function register();
+ */ 
+$timer_load = new BrexitTimer;
+$timer_load->register();
---
+++ js/timer.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to
+var countDownDate = new Date(brexit_string.uk_time).getTime();
+
+console.log(brexit_string.wording);
+console.log(brexit_string.uk_time);
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Set the id for timer
+    var timerID = "brexit-timer";
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById(timerID).innerHTML = brexit_string.wording + " (" + brexit_string.uk_time + ") is in " + days + " days " + hours + " hours " + minutes + " mins " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+    clearInterval(x);
+    document.getElementById(timerID).innerHTML = "Brexit already happened.";
+    }
+}, 1000);
