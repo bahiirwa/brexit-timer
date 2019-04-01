69ab64620871afd230b5ae9e506bca62bbb194e1
Laurence Bahiirwa <laurence@mediauganda.com>
2019-04-02 02:11:33 +0300

    Clean up js

--- js/timer.js
+++ js/timer.js
@@ -1,18 +1,12 @@
 // Set the date we're counting down to
 var countDownDate = new Date(brexit_string.uk_time).getTime();
 
-// console.log(brexit_string.wording);
-// console.log(brexit_string.uk_time);
-
 // Update the count down every 1 second
 var x = setInterval(function() {
-
     // Set the id for timer
     var timerID = "brexit-timer";
-
     // Get todays date and time
     var now = new Date().getTime();
-
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
@@ -27,7 +21,10 @@ var x = setInterval(function() {
 
     // If the count down is over, write some text 
     if (distance < 0) {
-    clearInterval(x);
-    document.getElementById(timerID).innerHTML = "Brexit already happened.";
+        clearInterval(x);
+        document.getElementById(timerID).innerHTML = "Brexit already happened.";
     }
 }, 1000);
+
+// console.log(brexit_string.wording);
+// console.log(brexit_string.uk_time);
